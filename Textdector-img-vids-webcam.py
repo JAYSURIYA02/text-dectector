09bd207a3512853c054668ddaf64b1a1c10587a6
JAYSURIYA02 <jaysuriya.rv@gmail.com>
2025-09-06 15:32:04 +0530

    finalised

--- Textdector-img-vids-webcam.py
+++ Textdector-img-vids-webcam.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 
 
 reader = easyocr.Reader(['en'], gpu=True)
-print("1.image\n2.video\n3.webcam")
+print("\n1.image\n2.video\n3.webcam")
 
 choice = input ('enter the choice ').strip()
 if(int(choice)==1):
@@ -23,8 +23,10 @@ if(int(choice)==1):
     for (bbox, text, score) in result:
         print(bbox, text, score)
         if score >threshold:
-            cv2.rectangle(img_bgr,bbox[0],bbox[2],(0,255,0),5)
-            cv2.putText(img_bgr,text,bbox[0],cv2.FONT_HERSHEY_COMPLEX,0.50,(0,0,255),2)
+            pt1 = tuple(map(int, bbox[0]))  
+            pt2 = tuple(map(int, bbox[2]))
+            cv2.rectangle(img_bgr, pt1, pt2, (0, 255, 0), 2)
+            cv2.putText(img_bgr, text, pt1, cv2.FONT_HERSHEY_COMPLEX, 0.65, (0, 0, 255), 2)
     plt.imshow(cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB))
     plt.axis('off')
     plt.show()
@@ -44,7 +46,7 @@ elif(int(choice) ==2 ):
                 pt1 = tuple(map(int, bbox[0]))  
                 pt2 = tuple(map(int, bbox[2]))
                 cv2.rectangle(img_bgr, pt1, pt2, (0, 255, 0), 2)
-                cv2.putText(img_bgr, text, pt1, cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 255), 2)
+                cv2.putText(img_bgr, text, pt1, cv2.FONT_HERSHEY_COMPLEX, 0.65, (0, 0, 255), 2)
         cv2.imshow("Video OCR", img_bgr)
 
     # Press 'q' to quit
@@ -67,7 +69,7 @@ elif(int(choice) == 3):
                 pt1 = tuple(map(int, bbox[0]))  
                 pt2 = tuple(map(int, bbox[2]))
                 cv2.rectangle(img_bgr, pt1, pt2, (0, 255, 0), 2)
-                cv2.putText(img_bgr, text, pt1, cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 255), 2)
+                cv2.putText(img_bgr, text, pt1, cv2.FONT_HERSHEY_COMPLEX, 0.65, (0, 0, 255), 2)
         cv2.imshow("Webcam OCR", img_bgr)
     # Press 'q' to quit
         if cv2.waitKey(1) & 0xFF == ord('q'):
